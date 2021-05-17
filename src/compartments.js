8504d384a61d1f6149fc1e887ea2b1e60887629c
Louise McMahon <louise@louisemcmahon.co.uk>
2021-05-18 00:42:57 +0100

    Feature/basic setup (#1)
    Create model setup functions and tests

---
+++ src/compartments.js
@@ -0,0 +1,17 @@
+module.exports = {
+    //static variables set inside the object for performance reasons
+    waterVapourPressure: undefined,
+    surfacePressure: undefined,
+
+    //Compartment variables
+    nitrogenPressure: undefined,
+    heliumPressure: undefined,
+    nitrogenCoefficientA: undefined,
+    nitrogenCoefficientB: undefined,
+    heliumCoefficientA: undefined,
+    heliumCoefficientB: undefined,
+    nitrogenHalfLife: undefined,
+    heliumHalfLife: undefined,
+
+    //function updateCompartmentPressure
+}
