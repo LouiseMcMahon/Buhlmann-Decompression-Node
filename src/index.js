660b49eb5bdad6e584e429b01cb9c0035bd272ad
Louise McMahon <louise@louisemcmahon.co.uk>
2021-05-18 01:00:15 +0100

    Add eslint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const compartments = require('./compartments')
-const static = require('./static')
+const compartments = require('./compartments');
+const staticData = require('./static-data');
 
 module.exports = {
-    gfLow: undefined,
-    gfHigh: undefined,
-    surfacePressure: undefined,
-    levels: [],
-    gas: [],
-    compartments: [],
+	gfLow: undefined,
+	gfHigh: undefined,
+	surfacePressure: undefined,
+	levels: [],
+	gas: [],
+	compartments: [],
 
-    /**
+	/**
      * Called to pass configuration variables for the model, should only be called once before computation
      *
      * @param gfLow Gradient factor low as percentage 0-1
@@ -17,73 +17,73 @@ module.exports = {
      * @param surfacePressure Dive site pressure in bar
      * @returns {exports}
      */
-    configureModel: function(gfLow, gfHigh, surfacePressure) {
-        if (isNaN(gfLow) || isNaN(gfHigh) || isNaN(surfacePressure)) {
-            throw new TypeError('Parameter is not a number');
-        }
+	configureModel: function(gfLow, gfHigh, surfacePressure) {
+		if (isNaN(gfLow) || isNaN(gfHigh) || isNaN(surfacePressure)) {
+			throw new TypeError('Parameter is not a number');
+		}
 
-        if (gfLow < 0 || gfLow > 1 || gfHigh < 0 || gfHigh > 1  ) {
-            throw new TypeError('Gradient factors must be a percentage expressed as a number between 0 and 1');
-        }
+		if (gfLow < 0 || gfLow > 1 || gfHigh < 0 || gfHigh > 1  ) {
+			throw new TypeError('Gradient factors must be a percentage expressed as a number between 0 and 1');
+		}
 
-        this.gfLow = gfLow;
-        this.gfHigh = gfHigh;
-        this.surfacePressure = surfacePressure
+		this.gfLow = gfLow;
+		this.gfHigh = gfHigh;
+		this.surfacePressure = surfacePressure;
 
-        return this;
-    },
+		return this;
+	},
 
-    addGas: function (oxygenPercent, heliumPercent){
-        if (isNaN(oxygenPercent) || isNaN(heliumPercent)) {
-            throw new TypeError('Parameter is not a number');
-        }
+	addGas: function (oxygenPercent, heliumPercent){
+		if (isNaN(oxygenPercent) || isNaN(heliumPercent)) {
+			throw new TypeError('Parameter is not a number');
+		}
 
-        this.gas.push([oxygenPercent, heliumPercent])
+		this.gas.push([oxygenPercent, heliumPercent]);
 
-        return this;
-    },
+		return this;
+	},
 
-    addLevel: function (endDepth, timePeriod){
-        if (isNaN(endDepth) || isNaN(timePeriod)) {
-            throw new TypeError('Parameter is not a number');
-        }
+	addLevel: function (endDepth, timePeriod){
+		if (isNaN(endDepth) || isNaN(timePeriod)) {
+			throw new TypeError('Parameter is not a number');
+		}
 
-        this.levels.push({
-            endDepth: endDepth,
-            timePeriod: timePeriod,
-            userInput: true
-        })
+		this.levels.push({
+			endDepth: endDepth,
+			timePeriod: timePeriod,
+			userInput: true
+		});
 
-        return this;
+		return this;
 
-    },
+	},
 
-    populateCompartments: function (){
-        if(this.compartments.length > 0){
-            throw new Error('Compartments are already populated')
-        }
+	populateCompartments: function (){
+		if(this.compartments.length > 0){
+			throw new Error('Compartments are already populated');
+		}
 
-        let i = 0;
-        while(i < 16){
-            const compartment = Object.create(compartments);
-            compartment.nitrogenCoefficientA = static.nitrogenCoefficientA[i]
-            compartment.nitrogenCoefficientB = static.nitrogenCoefficientB[i]
-            compartment.heliumCoefficientA = static.heliumCoefficientA[i]
-            compartment.heliumCoefficientB = static.heliumCoefficientB[i]
-            compartment.nitrogenHalfLife = static.nitrogenHalfLife[i]
-            compartment.heliumHalfLife = static.heliumHalfLife[i]
-            compartment.waterVapourPressure = static.waterVapourPressure
-            compartment.surfacePressure = this.surfacePressure
-            compartment.nitrogenPressure = static.initialNitrogenPressure * (this.surfacePressure - static.waterVapourPressure)
-            compartment.heliumPressure = static.initialHeliumPressure
-            this.compartments.push(compartment)
-            i++
-        }
+		let i = 0;
+		while(i < 16){
+			const compartment = Object.create(compartments);
+			compartment.nitrogenCoefficientA = staticData.nitrogenCoefficientA[i];
+			compartment.nitrogenCoefficientB = staticData.nitrogenCoefficientB[i];
+			compartment.heliumCoefficientA = staticData.heliumCoefficientA[i];
+			compartment.heliumCoefficientB = staticData.heliumCoefficientB[i];
+			compartment.nitrogenHalfLife = staticData.nitrogenHalfLife[i];
+			compartment.heliumHalfLife = staticData.heliumHalfLife[i];
+			compartment.waterVapourPressure = staticData.waterVapourPressure;
+			compartment.surfacePressure = this.surfacePressure;
+			compartment.nitrogenPressure = staticData.initialNitrogenPressure * (this.surfacePressure - staticData.waterVapourPressure);
+			compartment.heliumPressure = staticData.initialHeliumPressure;
+			this.compartments.push(compartment);
+			i++;
+		}
 
-        return this
-    },
+		return this;
+	},
 
-    compute: function (){
-        return this;
-    }
-}
+	compute: function (){
+		return this;
+	}
+};
